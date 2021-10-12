0c7c75fff1bc69f525a710d1df8b50a571178e98
Ao Shen <ao.shen0508@gmail.com>
2021-10-12 14:55:58 -0400

    update setter

--- model/gd.py
+++ model/gd.py
@@ -2,61 +2,77 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 class LogisticRegression:
-
-    def __init__(self, add_bias=True, learning_rate=.1, epsilon=1e-4, max_iters=1e5, verbose=False):
+    
+    def __init__(self, add_bias=True, learning_rate=.1, epsilon=1e-4, max_iters=1e5, verbose=False, mini_batch=1, momentum=1):
         self.add_bias = add_bias
         self.learning_rate = learning_rate
-        self.epsilon = epsilon  # to get the tolerance for the norm of gradients
-        self.max_iters = max_iters  # maximum number of iteration of gradient descent
+        self.epsilon = epsilon                        #to get the tolerance for the norm of gradients 
+        self.max_iters = max_iters                    #maximum number of iteration of gradient descent
         self.verbose = verbose
-
+        self.mini_batch = mini_batch
+        self.momentum = momentum
+        
     def fit(self, x, y):
         if x.ndim == 1:
             x = x[:, None]
         if self.add_bias:
             N = x.shape[0]
-            x = np.column_stack([x, np.ones(N)])
-        N, D = x.shape
+            x = np.column_stack([x,np.ones(N)])
+        N,D = x.shape
         self.w = np.zeros(D)
-        g = np.inf
+        g = np.inf 
         t = 0
         # the code snippet below is for gradient descent
         while np.linalg.norm(g) > self.epsilon and t < self.max_iters:
             g = self.gradient(x, y)
-            self.w = self.w - self.learning_rate * g
+            self.w = self.w - self.learning_rate * g 
             t += 1
-
+        
         if self.verbose:
             print(f'terminated after {t} iterations, with norm of the gradient equal to {np.linalg.norm(g)}')
             print(f'the weight found: {self.w}')
         return self
-
+    
     def predict(self, x):
         if x.ndim == 1:
             x = x[:, None]
         Nt = x.shape[0]
         if self.add_bias:
-            x = np.column_stack([x, np.ones(Nt)])
-        yh = logistic(np.dot(x, self.w))  # predict output
+            x = np.column_stack([x,np.ones(Nt)])
+        yh = logistic(np.dot(x,self.w))            #predict output
         return yh
+    def get_params(self):
+        return 
+    def set_params(self, new_params: Dict[str, Any]):
+        for k, v in new_params.items():
+            if k == 'add_bias':
+                self.add_bias = v
+            else if k == 'learning_rate':
+                self.learning_rate = v
+            else if k == 'epsilon':
+                self.epsilon = v
+            else if k == 'max_iters': 
+                self.max_iters = v
+            else if k == 'verbose':
+                self.verbose = v
+            else if k == 'mini_batch':
+                self.mini_batch = v
+            else if k == 'momentum':
+                self.momentum = v
+        return True
 
 
-LogisticRegression.gradient = gradient  # initialize the gradient method of the LogisticRegression class with gradient function
-
+LogisticRegression.gradient = gradient             #initialize the gradient method of the LogisticRegression class with gradient function
 
 def cost_fn(x, y, w):
-    N, D = x.shape
+    N, D = x.shape                                                       
     z = np.dot(x, w)
-    J = np.mean(y * np.log1p(np.exp(-z)) + (1 - y) * np.log1p(
-        np.exp(z)))  # log1p calculates log(1+x) to remove floating point inaccuracies
+    J = np.mean(y * np.log1p(np.exp(-z)) + (1-y) * np.log1p(np.exp(z)))  #log1p calculates log(1+x) to remove floating point inaccuracies 
     return J
 
-
 def gradient(self, x, y):
-    N, D = x.shape
-    yh = logistic(np.dot(x, self.w))  # predictions  size N
-    grad = np.dot(x.T, yh - y) / N  # divide by N because cost is mean over N points
-    return grad
-
+    N,D = x.shape
+    yh = logistic(np.dot(x, self.w))    # predictions  size N
+    grad = np.dot(x.T, yh - y)/N        # divide by N because cost is mean over N points
+    return grad   
