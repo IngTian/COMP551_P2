aa1d3f296dc54a9710a71a1bdede0e9d2276739b
IngTian <tzy20001018@gmail.com>
2021-10-12 16:40:08 -0400

    feat: completed

--- part1.py
+++ part1.py
@@ -1,28 +1,33 @@
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-
+from utils.io_utils import read_csv
 from model.gd import LogisticRegression
+from utils.utils import get_best_model_parameter, cross_validate
 
-diabetes_train = pd.read_csv("data_sets/data_A2/diabetes/diabetes_train.csv")
+# region Read Data
+training_data = read_csv("data_sets/data_A2/diabetes/diabetes_train.csv").to_numpy()
+test_data = read_csv("data_sets/data_A2/diabetes/diabetes_test.csv").to_numpy()
+val_data = read_csv("data_sets/data_A2/diabetes/diabetes_val.csv").to_numpy()
 
+# endregion
 
-def process_dataset(data):
-    data=data.to_numpy()
-    x, y =data[:,:-1],data[:,-1:]
-    y=np.squeeze(y)
-    return(x,y)
+params = {
+    "learning_rate": [0.01, 0.05, 0.1, 0.15, 0.2, 0.3],
+    "max_iterations": [100, 300, 500, 1000, 2000, 5000, 10000, 50000, 100000],
+    "mini_batch_ratio": [1, 0.01, 0.02, 0.05, 0.1, 0.2, 0.4, 0.5],
+    "momentum": [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.1],
+    "update_weight_method": [
+        UpdateWeightMethod.REGULAR,
+        UpdateWeightMethod.MOMENTUM
+    ]
+}
 
-diabetes_train=process_dataset(diabetes_train)
-x=diabetes_train[0]
-y=diabetes_train[1]
+best_param, results = get_best_model_parameter(
+    params,
+    LogisticRegression,
+    training_data[:, :-1],
+    training_data[:, -1],
+    cross_validate,
+    val_x=val_data[:, :-1],
+    val_y=val_data[:, -1]
+)
 
-                              #generate synthetic data
-model = LogisticRegression(verbose=True, )
-yh = model.fit(x,y).predict(x)
-plt.plot(x, y, '.', label='dataset')
-plt.plot(x, yh, 'g', alpha=.5, label='predictions')
-plt.xlabel('x')
-plt.ylabel(r'$y$')
-plt.legend()
-plt.show()
+# TODO save the results above.
