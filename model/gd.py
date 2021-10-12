573f2883b2e90a19acb0699388e71810d989d83d
IngTian <tzy20001018@gmail.com>
2021-10-12 16:10:47 -0400

    feat: completed logistic regression model

--- model/gd.py
+++ model/gd.py
@@ -1,78 +1,164 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+from utils.math_utils import sigmoid
+from typing import Dict, Tuple, List, Any
+from enum import Enum
+from simple_chalk import chalk
+
+
+class UpdateWeightMethod(Enum):
+    REGULAR = 1
+    MOMENTUM = 2
+
 
 class LogisticRegression:
-    
-    def __init__(self, add_bias=True, learning_rate=.1, epsilon=1e-4, max_iters=1e5, verbose=False, mini_batch=1, momentum=1):
+
+    def __init__(
+            self,
+            add_bias: bool = True,
+            learning_rate: float = .1,
+            epsilon: float = 1e-4,
+            max_iterations: int = 1e5,
+            verbose: bool = True,
+            mini_batch_ratio: float = 1,
+            momentum: Tuple[float, int] = None,
+            update_weight_method: UpdateWeightMethod = UpdateWeightMethod.REGULAR
+    ):
         self.add_bias = add_bias
         self.learning_rate = learning_rate
-        self.epsilon = epsilon                        #to get the tolerance for the norm of gradients 
-        self.max_iters = max_iters                    #maximum number of iteration of gradient descent
-        self.verbose = verbose
-        self.mini_batch = mini_batch
+        self.epsilon = epsilon  # to get the tolerance for the norm of gradients
+        self.max_iterations = max_iterations  # maximum number of iteration of gradient descent
+        self.mini_batch_ratio = mini_batch_ratio
         self.momentum = momentum
-        
+        self.update_weights_method = update_weight_method
+        self.verbose = verbose
+
+        # Model Parameters.
+        self.weights = None
+        self.history_gradients = None
+
     def fit(self, x, y):
+
+        # Prepare X
         if x.ndim == 1:
             x = x[:, None]
         if self.add_bias:
-            N = x.shape[0]
-            x = np.column_stack([x,np.ones(N)])
-        N,D = x.shape
-        self.w = np.zeros(D)
-        g = np.inf 
-        t = 0
+            number_of_instances = x.shape[0]
+            x = np.column_stack([x, np.ones(number_of_instances)])
+
+        # Get the segmented training
+        # sets based on batch size.
+        training_sets = self.separate_training_data(x, y)
+
+        # Set up parameters
+        number_of_instances, number_of_features = x.shape
+        self.weights = np.zeros(number_of_features)
+        self.history_gradients = list()
+        current_gradient, iterations_run = np.inf, 0
+
         # the code snippet below is for gradient descent
-        while np.linalg.norm(g) > self.epsilon and t < self.max_iters:
-            g = self.gradient(x, y)
-            self.w = self.w - self.learning_rate * g 
-            t += 1
-        
+        while np.linalg.norm(current_gradient) > self.epsilon and iterations_run < self.max_iterations:
+            training_set = training_sets[iterations_run % len(training_sets)]
+            current_gradient = self.gradient(training_set[0], training_set[1])
+            self.history_gradients.append(current_gradient)
+            self.update_weights(current_gradient)
+            iterations_run += 1
+
         if self.verbose:
-            print(f'terminated after {t} iterations, with norm of the gradient equal to {np.linalg.norm(g)}')
-            print(f'the weight found: {self.w}')
+            print(
+                f'{chalk.bold("-" * 15 + "COMPLETED FITTING" + "-" * 15)}\n'
+                f'NUMBER OF ITERATIONS: {chalk.green.bold(iterations_run)} FINAL GRADIENT NORM: {chalk.yellowBright.bold(np.linalg.norm(current_gradient))}\n'
+                f'FINAL WEIGHTS: {chalk.blueBright(self.weights)}\n')
+
         return self
-    
+
+    def update_weights(self, raw_gradients: np.ndarray) -> None:
+        """
+        Given the raw_gradients
+        :param raw_gradients:
+        :return:
+        """
+        if self.update_weights_method == UpdateWeightMethod.MOMENTUM:
+            # TODO: Use momentum to update weights
+            pass
+        elif self.update_weights_method == UpdateWeightMethod.REGULAR:
+            # TODO: Update weights regularly
+            pass
+
+    def separate_training_data(
+            self,
+            x: np.ndarray,
+            y: np.ndarray,
+    ) -> List[Tuple[np.ndarray, np.ndarray]]:
+        """
+        Separate the training data into
+        segments according to the batch size.
+        :param self:
+        :param x: X
+        :param y: y
+        :return: A list of segmented batches.
+        """
+        complete_data = np.append(x if x.ndim > 1 else x[:, None], y if y.ndim > 1 else y[:, None], axis=1)
+        batch_size = int(x.shape[0] * self.mini_batch_ratio)
+
+        if batch_size > x.shape[0]:
+            raise ValueError("Batch size is larger than the number of instances.")
+
+        result = list()
+
+        while complete_data.shape[0] > 0:
+            if complete_data.shape[0] >= batch_size:
+                result.append((complete_data[:batch_size, :-1], complete_data[:batch_size, -1]))
+                complete_data = complete_data[batch_size:]
+
+        return result
+
     def predict(self, x):
+        """
+        Make predictions based on the weights given.
+        :param x: X
+        :return: The predicted y.
+        """
+        # Prepare X
         if x.ndim == 1:
             x = x[:, None]
-        Nt = x.shape[0]
+        number_of_tests = x.shape[0]
         if self.add_bias:
-            x = np.column_stack([x,np.ones(Nt)])
-        yh = logistic(np.dot(x,self.w))            #predict output
-        return yh
+            x = np.column_stack([x, np.ones(number_of_tests)])
+
+        # Make predictions
+        return sigmoid(np.dot(x, self.weights))
+
     def get_params(self):
-        return 
+        return self.__dict__
+
     def set_params(self, new_params: Dict[str, Any]):
         for k, v in new_params.items():
             if k == 'add_bias':
                 self.add_bias = v
-            else if k == 'learning_rate':
+            elif k == 'learning_rate':
                 self.learning_rate = v
-            else if k == 'epsilon':
+            elif k == 'epsilon':
                 self.epsilon = v
-            else if k == 'max_iters': 
-                self.max_iters = v
-            else if k == 'verbose':
+            elif k == 'max_iterations':
+                self.max_iterations = v
+            elif k == 'verbose':
                 self.verbose = v
-            else if k == 'mini_batch':
-                self.mini_batch = v
-            else if k == 'momentum':
+            elif k == 'mini_batch_ratio':
+                self.mini_batch_ratio = v
+            elif k == 'momentum':
                 self.momentum = v
+            elif k == 'update_weights_method':
+                self.update_weights_method = v
         return True
 
-
-LogisticRegression.gradient = gradient             #initialize the gradient method of the LogisticRegression class with gradient function
-
-def cost_fn(x, y, w):
-    N, D = x.shape                                                       
-    z = np.dot(x, w)
-    J = np.mean(y * np.log1p(np.exp(-z)) + (1-y) * np.log1p(np.exp(z)))  #log1p calculates log(1+x) to remove floating point inaccuracies 
-    return J
-
-def gradient(self, x, y):
-    N,D = x.shape
-    yh = logistic(np.dot(x, self.w))    # predictions  size N
-    grad = np.dot(x.T, yh - y)/N        # divide by N because cost is mean over N points
-    return grad   
+    def gradient(self, x, y):
+        """
+        Calculate the gradient of logistic regression.
+        :param x:
+        :param y:
+        :return:
+        """
+        number_of_instances, number_of_features = x.shape
+        yh = sigmoid(np.dot(x, self.weights))  # predictions  size N
+        grad = np.dot(x.T, yh - y) / number_of_instances  # divide by N because cost is mean over N points
+        return grad
