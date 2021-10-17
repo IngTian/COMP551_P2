015dc698d831a783bc184d06e8f97bb579328d4b
IngTian <tzy20001018@gmail.com>
2021-10-16 22:27:44 -0400

    fix: edited episilon

--- lib/model/gd.py
+++ lib/model/gd.py
@@ -11,7 +11,7 @@ class LogisticRegression(LearningModel):
             self,
             add_bias: bool = True,
             learning_rate: float = .1,
-            epsilon: float = 1e-4,
+            epsilon: float = 1e-3,
             verbose: bool = False,
             mini_batch: int = None,
             momentum: float = None,
