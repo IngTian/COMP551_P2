f8353d37ed91a2ed64c626927f0afbbca241d139
IngTian <tzy20001018@gmail.com>
2021-10-20 22:52:59 -0400

    feat: eidted epsilon to 6.5e-3

--- lib/model/gd.py
+++ lib/model/gd.py
@@ -15,7 +15,7 @@ class LogisticRegression(LearningModel):
             self,
             add_bias: bool = True,
             learning_rate: float = .1,
-            epsilon: float = 2.5e-2,
+            epsilon: float = 6.5e-3,
             verbose: bool = False,
             mini_batch: int = None,
             momentum: float = None,
