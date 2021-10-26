151ed416b01fafea47bddc06b6f4e386f009f05e
IngTian <tzy20001018@gmail.com>
2021-10-26 13:00:26 -0400

    modified part2.py

--- part2.py
+++ part2.py
@@ -1,102 +1,34 @@
 import json
 from lib.utils.io_utils import read_csv
-from lib.utils.utils import preprocess_data, get_best_model_parameter, cross_validate
-from typing import List, Tuple, Callable, Dict, Any, Union
+from typing import Dict
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import classification_report
 from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
 from tqdm import tqdm
 import nltk
 
-nltk.download('stopwords')
 nltk.download('wordnet')
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 from nltk.stem import SnowballStemmer
 import numpy as np
 from simple_chalk import chalk
 
 
 # region Preprocess Data Functions
-def preprocess_corpora(
-        training_corpus: np.ndarray,
-        validation_corpus: np.ndarray,
-        test_corpus: np.ndarray,
-        vectorizer: CountVectorizer
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    training_x, training_y = training_corpus[:, 0], training_corpus[:, -1]
-    validation_x, validation_y = validation_corpus[:, 0], validation_corpus[:, -1]
-    test_x, test_y = test_corpus[:, 0], test_corpus[:, -1]
-
-    transformed_training_x = vectorizer.transform(training_x).toarray()
-    transformed_validation_x = vectorizer.transform(validation_x).toarray()
-    transformed_test_x = vectorizer.transform(test_x).toarray()
-
-    transformed_training = np.append(
-        transformed_training_x if transformed_training_x.ndim > 1 else transformed_training_x[:, np.newaxis],
-        training_y if training_y.ndim > 1 else training_y[:, np.newaxis], axis=1)
-    transformed_validation = np.append(
-        transformed_validation_x if transformed_validation_x.ndim > 1 else transformed_validation_x[:, np.newaxis],
-        validation_y if validation_y.ndim > 1 else validation_y[:, np.newaxis], axis=1)
-    transformed_test = np.append(
-        transformed_test_x if transformed_test_x.ndim > 1 else transformed_test_x[:, np.newaxis],
-        test_y if test_y.ndim > 1 else test_y[:, np.newaxis], axis=1)
-
-    return transformed_training, transformed_validation, transformed_test
-
-
-def preprocess_get_unigram_vectorizer(
-        corpus: np.ndarray,
-        params: Dict = None
-) -> CountVectorizer:
-    vectorizer = CountVectorizer(**params)
-    vectorizer.fit(corpus)
-    return vectorizer
-
-
-def preprocess_get_unigram_ecl_stopwords_vectorizer(
-        corpus: np.ndarray,
-        params: Dict = None
-) -> CountVectorizer:
-    vectorizer = CountVectorizer(**params, stop_words=stopwords.words('english'))
-    vectorizer.fit(corpus)
-    return vectorizer
-
-
-def preprocess_get_unigram_with_stemming_vectorizer(
+def preprocess_get_vectorizer_with_stemming(
         corpus: np.ndarray,
         params: Dict = None
 ) -> CountVectorizer:
     stemming_helper = SnowballStemmer('english')
     vectorizer = CountVectorizer(**params)
-    vectorizer.fit(corpus)
+    vectorizer.fit(corpus[:, 0], corpus[:, 1])
     analyzer = vectorizer.build_analyzer()
 
     new_vectorizer = CountVectorizer(
         **params,
         analyzer=lambda s: (stemming_helper.stem(word) for word in analyzer(s)),
     )
-
-    new_vectorizer.fit(corpus)
-
-    return new_vectorizer
-
-
-def preprocess_get_unigram_with_lemmatization_vectorizer(
-        corpus: np.ndarray,
-        params: Dict = None
-) -> CountVectorizer:
-    lemmatizer = WordNetLemmatizer()
-    vectorizer = CountVectorizer(**params)
-    vectorizer.fit(corpus)
-    analyzer = vectorizer.build_analyzer()
-
-    new_vectorizer = CountVectorizer(
-        **params,
-        analyzer=lambda s: (lemmatizer.lemmatize(word) for word in analyzer(s)),
-    )
-
-    new_vectorizer.fit(corpus)
 
     return new_vectorizer
 
@@ -114,52 +46,281 @@ if __name__ == '__main__':
     val_data = read_csv("data_sets/data_A2/fake_news/fake_news_val.csv").to_numpy()
     print(f'SHAPE: {chalk.greenBright(val_data.shape)}')
 
-    # Preprocess Data
-    max_features_dict = {
-        'max_features': 10000
-    }
-
-    vectorizer_generators = [
-        preprocess_get_unigram_vectorizer,
-        preprocess_get_unigram_ecl_stopwords_vectorizer,
-        preprocess_get_unigram_with_stemming_vectorizer,
-        preprocess_get_unigram_with_lemmatization_vectorizer
+    params = [
+        {
+            "stemming": False,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": False,
+                "lowercase": True,
+                "ngram_range": (1, 1)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": False,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": False,
+                "lowercase": True,
+                "ngram_range": (1, 2)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": False,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": False,
+                "lowercase": False,
+                "ngram_range": (1, 1)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": False,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": False,
+                "lowercase": False,
+                "ngram_range": (1, 2)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": False,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": True,
+                "lowercase": True,
+                "ngram_range": (1, 1)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": False,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": True,
+                "lowercase": True,
+                "ngram_range": (1, 2)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": False,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": True,
+                "lowercase": False,
+                "ngram_range": (1, 1)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": False,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": True,
+                "lowercase": False,
+                "ngram_range": (1, 2)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": True,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": False,
+                "lowercase": True,
+                "ngram_range": (1, 1)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": True,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": False,
+                "lowercase": True,
+                "ngram_range": (1, 2)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": True,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": False,
+                "lowercase": False,
+                "ngram_range": (1, 1)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": True,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": False,
+                "lowercase": False,
+                "ngram_range": (1, 2)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": True,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": True,
+                "lowercase": True,
+                "ngram_range": (1, 1)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": True,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": True,
+                "lowercase": True,
+                "ngram_range": (1, 2)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": True,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": True,
+                "lowercase": False,
+                "ngram_range": (1, 1)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
+        {
+            "stemming": True,
+            "use_tfidf": False,
+            "vectorizer_params": {
+                "binary": True,
+                "lowercase": False,
+                "ngram_range": (1, 2)
+            },
+            "tfidf_params": {},
+            "clf_params": {
+                "max_iter": 1000,
+                "solver": "sag"
+            }
+        },
     ]
 
-    processed_data_sets: List[Tuple[np.ndarray, np.ndarray, np.ndarray]] = list()
+    # Running results
+    results = []
 
-    print(f'{chalk.bold("-" * 15 + "START PREPROCESSING" + "-" * 15)}\n')
-    for generator in tqdm(vectorizer_generators):
-        vectorizer = generator(training_data[:, 0], max_features_dict)
-        processed_data_sets.append(preprocess_corpora(training_data, val_data, test_data, vectorizer))
+    for param_combination in tqdm(params):
+        use_stemming = param_combination['stemming']
+        use_tfidf = param_combination['use_tfidf']
+        tfidf_params = param_combination['tfidf_params']
+        vectorizer_params = param_combination['vectorizer_params']
+        clf_params = param_combination['clf_params']
 
-    print(f'\n\n{chalk.bold("-" * 15 + "DATA PREPROCESS COMPLETED" + "-" * 15)}\n')
+        vectorizer, text_clf = None, None
 
-    # Run grid search for each preprocessed data
-    # and pick the best combination
-    results: Dict[str, Any] = dict()
+        if use_stemming:
+            vectorizer = preprocess_get_vectorizer_with_stemming(training_data, vectorizer_params)
+        else:
+            vectorizer = CountVectorizer(**vectorizer_params)
 
-    print(f'\n\n{chalk.bold("-" * 15 + "STARTING RUNNING TESTS" + "-" * 15)}\n')
+        if not use_tfidf:
+            text_clf = Pipeline([
+                ("vect", vectorizer),
+                ("clf", LogisticRegression(**clf_params))
+            ])
+        else:
+            text_clf = Pipeline([
+                ("vect", vectorizer),
+                ("tfidf", TfidfTransformer(**tfidf_params)),
+                ("clf", LogisticRegression(**clf_params))
+            ])
 
-    for generator_idx in tqdm(range(len(vectorizer_generators))):
-        preprocess = vectorizer_generators[generator_idx]
-        train_data, val_data, test_data = processed_data_sets[generator_idx]
-        name_of_the_preprocess = preprocess.__name__
+        text_clf.fit(training_data[:, 0], training_data[:, 1].astype(int))
 
-        model = LogisticRegression(max_iter=1000)
-        model.fit(train_data[:, :-1].astype(int), train_data[:, -1].astype(int))
-
-        validation_report = classification_report(
+        result = {}
+        result["params"] = param_combination
+        result["val"] = classification_report(
             val_data[:, -1].astype(int),
-            model.predict(val_data[:, :-1].astype(int)),
+            text_clf.predict(val_data[:, 0]).astype(int),
+            output_dict=True,
+            zero_division=0
+        )
+        result["test"] = classification_report(
+            test_data[:, -1].astype(int),
+            text_clf.predict(test_data[:, 0]).astype(int),
             output_dict=True,
             zero_division=0
         )
 
-        results[name_of_the_preprocess] = validation_report
+        results.append(result)
 
-    # Save results
-    print(f'\n\n{chalk.bold("-" * 15 + "COMPLETED" + "-" * 15)}\n')
     f = open("./output/part2.json", 'w')
     f.write(json.dumps(results, indent=4))
     f.close()
